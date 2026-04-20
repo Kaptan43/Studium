8dcd286cf71628eef0332c4d911b0270b6e8a640
Kaptan43 <mehmet_emre.korkmaz@outlook.de>
2026-04-20 21:59:30 +0200

    Initial commit

---
+++ src/uebung03_2025/Polynomial.java
@@ -0,0 +1,26 @@
+package uebung03_2025;
+
+public class Polynomial {
+    private final int[] polynom;
+    private final int degree;
+
+    public Polynomial(int[] polynom){
+        this.polynom = polynom;
+        int tempDegree = 0;
+        for(int i = polynom.length-1; i>=0; i--){
+            if(polynom[i] != 0){
+                tempDegree = i;
+                break;
+            }
+        }
+        this.degree = tempDegree;
+    }
+
+    public Polynomial(){
+        this(new int[]{0, 1});
+    }
+
+    public static Polynomial constant(int v){
+        return new Polynomial(new int[]{v});
+    }
+}
